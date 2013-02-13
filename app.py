2546599ab10a00cc1dc52f19d85c645f75126fc6
jc <chengjoanne2@gmail.com>
2013-02-12 20:29:30 -0700

    allow access control origin

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def closures():
   scraperwiki_query = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=denver_streets_and_sidewalks&query=select%20*%20from%20%60swdata%60%20where%20start_date%20%3C%20date('"+ d +"')%20and%20end_date%20%3E%20date('" + d + "')"
 
   scraperwiki_response = { 'items': json.loads(urllib2.urlopen(scraperwiki_query).read()) }
-  response = Response(json.dumps(scraperwiki_response), status=200, mimetype='application/json')
+  response = Response(json.dumps(scraperwiki_response), status=200, mimetype='application/json', headers={'Access-Control-Allow-Origin':'*'})
   return response
 
 @app.route('/closures/<int:closure_id>')
