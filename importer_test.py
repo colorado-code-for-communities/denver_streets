98b5b98069a06776f995af15d5c7ff4e31cf3ccd
jc <chengjoanne2@gmail.com>
2013-04-21 12:11:59 -0600

    move importer test file around, remove unneeded files

--- importer_test.py
+++ importer_test.py
@@ -1,15 +1,16 @@
 import unittest
 import json, os
-import importer
+from lib import importer
 
 # read_closures() should return import_test_fixtures.txt
 fixtures_dir = os.getcwd() + "/tests/fixtures"
 
 class ImporterTests(unittest.TestCase):
     def setUp(self):
-        self.closure_json = open(fixtures_dir + '/importer_test_fixtures.txt')
+        self.closure_json = json.loads(open(fixtures_dir + '/importer_test_fixtures.txt').read())
 
     def testImportClosure(self):
+        print self.closure_json
         importer.import_closures()
         assert(true).equals(true)
         #assert(Clo
