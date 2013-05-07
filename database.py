9cb06ee6af31edd273fd5c4b7787b852212459de
Joanne Cheng <joanne@thoughtbot.com>
2013-05-07 12:56:11 -0600

    Add postgis setup to database.setup_db()

--- database.py
+++ database.py
@@ -9,9 +9,10 @@ config = yaml.load(open('config.yaml', 'r'))
 
 try:
     if os.environ['FLASK_ENV'] == 'test':
-        databse_name = config['database']['test']['db']
+        database_name = config['database']['test']['db']
         database_user = config['database']['test']['user']
-        database_pass = ''
+        database_pass = config['database']['test']['pass']
+
     else:
         database_name = config['database']['development']['db']
         database_user = config['database']['development']['user']
@@ -39,10 +40,20 @@ def drop_db():
     metadata.drop_all(bind=engine)
 
 def setup_db():
-    engine = create_engine('postgresql://postgres@localhost/postgres')
-    conn = engine.connect()
+    postgres_engine = create_engine('postgresql://postgres@localhost/postgres')
+    conn = postgres_engine.connect()
     conn.execute('commit')
     conn.execute('create database ' + database_name)
+    conn.close()
+    os.system('psql -d ' + database_name + ' -f ' + config['database']['postgis_extensions_dir'] + '/postgis.sql')
+    os.system('psql -d ' + database_name + ' -f ' + config['database']['postgis_extensions_dir'] + '/spatial_ref_sys.sql')
+
+    admin_engine = create_engine('postgresql://postgres@localhost/' + database_name)
+    conn = admin_engine.connect()
+    conn.execute('grant all on database ' + database_name + ' to "' + database_user + '"')
+    conn.execute('grant all on spatial_ref_sys to "' + database_user + '"')
+    conn.execute('grant all on geometry_columns to "' + database_user + '"')
+    conn.execute('commit')
     conn.close()
 
 def destroy_db():
