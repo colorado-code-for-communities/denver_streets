5ea5a7c9617a20a4f8040fa91444edfeaddeaad8
Joanne Cheng <joanne@thoughtbot.com>
2013-05-09 16:31:14 -0600

    remove name column from models

--- models.py
+++ models.py
@@ -10,7 +10,6 @@ Base = denver_streets.database.Base
 class Closure(Base):
     __tablename__ = 'closures'
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True)
     location = Column(String(256), unique=True)
     closure_type = Column(String(255))
     purpose = Column(String(255))
