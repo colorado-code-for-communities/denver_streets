b8144069b2ef27526791b32b31da68284557ec1f
Joanne Cheng <joanne@thoughtbot.com>
2013-08-27 21:04:05 -0600

    import *all* closings from scraperwiki

--- lib/importer.py
+++ lib/importer.py
@@ -13,7 +13,7 @@ street_parser = StreetParser()
 
 def read_closings():
     d = datetime.datetime.today().strftime('%Y-%m-%d')
-    scraperwiki_query = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=denver_streets_and_sidewalks&query=select%20*%20from%20%60swdata%60%20where%20start_date%20%3C%20date('"+ d +"')%20and%20end_date%20%3E%20date('" + d + "')"
+    scraperwiki_query = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=denver_streets_and_sidewalks&query=select%20*%20from%20%60swdata%60"
     r = requests.get(scraperwiki_query)
     return r.text
 
